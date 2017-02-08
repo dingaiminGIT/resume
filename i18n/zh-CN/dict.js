adbde3d81bf6a84cd85eddf8654e0b42e243a2b4
wxweven <wxweven@gmail.com>
2017-02-08 21:06:12 +0800

    init resume

---
+++ i18n/zh-CN/dict.js
@@ -0,0 +1,49 @@
+module.exports = {
+    experience_type: {
+        "internship": "（实习）",
+        "work": "（正式）"
+    },
+    skill_type: {
+        "frontend": "Web 前端",
+        "java": "Java",
+        "backend": "Web 后端",
+        "miscellaneous": "其他"
+    },
+    education_type: {
+        "bachelor": "（学士）",
+        "master": "（学士 硕士）",
+        "doctor": "（博士）"
+    },
+    link_type: {
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣读书",
+        "repo": "源代码",
+        "pdf": "PDF 下载"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育经历",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "英语水平",
+            "en": "English Level"
+        },
+        "experience": {
+            "ch": "工作经历",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "专业技能",
+            "en": "Programming Skills"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~",
+    dict: {
+        source_code: "源代码",
+        demo: "Demo"
+    },
+    project_type: {
+        "personal": "个人项目"
+    }
+};
